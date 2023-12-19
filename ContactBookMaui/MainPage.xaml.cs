f9050748b7ed71b01c20aed3f5d1b8329a239620
Levi Stark <levi.john.stark@outlook.com>
2023-12-19 16:08:37 +0100

    Added MAUI project to Solution

---
+++ ContactBookMaui/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace ContactBookMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        private List<string> _items = [];
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Add_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(Entry_Name.Text))
+            {
+                _items.Add(Entry_Name.Text);
+                Entry_Name.Text = string.Empty;
+            }
+        }
+    }
+
+}
---
+++ ContactBook_Maui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ContactBook_Maui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
